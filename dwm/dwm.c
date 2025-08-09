f324867551481bb5f7f7eaa0fd36d2abc6dea32d
Tarek-L <tarek.laib24@gmail.com>
2025-08-09 20:23:00 +0100

    config

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1747,14 +1747,8 @@ void setfullscreen(Client *c, int fullscreen) {
 }
 Layout *last_layout;
 void fullscreen(const Arg *arg) {
-  if (selmon->showbar) {
-    for (last_layout = (Layout *)layouts;
-         last_layout != selmon->lt[selmon->sellt]; last_layout++)
-      ;
-    setlayout(&((Arg){.v = &layouts[2]}));
-  } else {
-    setlayout(&((Arg){.v = last_layout}));
-  }
+  if (selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
   togglebar(arg);
 }
 
